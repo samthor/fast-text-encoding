71650cafeb26ace6e31a96f493259ab380aa896b
Sam Thorogood <sam.thorogood@gmail.com>
2020-03-06 14:46:31 +1100

    don't clone buffer if we don't need to

--- text.js
+++ text.js
@@ -141,6 +141,13 @@ FastTextDecoder.prototype.decode = function(buffer, options={stream: false}) {
     throw new Error(`Failed to decode: the 'stream' option is unsupported.`);
   }
 
+  // Look for ArrayBufferView, which isn't a real type, but basically represents
+  // all the valid TypedArray types plus DataView. They all have ".buffer" as
+  // an instance of ArrayBuffer.
+  if (buffer.buffer instanceof ArrayBuffer) {
+    buffer = buffer.buffer;
+  }
+
   const bytes = new Uint8Array(buffer);
   let pos = 0;
   const len = bytes.length;
